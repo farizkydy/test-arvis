91cfd134c202b097f5fd6165ea2dfa1592d3d079
farizkydy <farizrizkyrizaldy@gmail.com>
2021-02-14 11:37:36 +0700

    Commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBpf4CBm4z6KzWPj0QSBGraeV3rDFC_MU8",
+  authDomain: "test-arvisid.firebaseapp.com",
+  projectId: "test-arvisid",
+  storageBucket: "test-arvisid.appspot.com",
+  messagingSenderId: "164705836569",
+  appId: "1:164705836569:web:e8b1d4a2d9d7a6eb0b5488"
+};
